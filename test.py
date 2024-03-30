b94949116f26ba33df163ab13770078365795332
Patryk Żak <patryszak@hotmail.com>
2024-03-30 18:16:12 +0100

    Somewhat working draft

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
+#import seaborn as sns
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
 
 # Meat and Fish
 meat_fish_keywords = ['Chicken', 'Fish', 'Beef', 'Pork', 'Salmon', 'Cod', 
@@ -79,29 +80,39 @@ spices_keywords = ['Black pepper', 'Cayenne pepper', 'Chili powder', 'Cumin', 'C
                    'Sriracha', 'Habanero', 'Chipotle', 'Ghost pepper', 'Scotch bonnet']
 
 
-# Initializations and downloads
-stop_words = set(stopwords.words('english'))
+def find_keyword_list(user_input):
+    user_input = user_input.lower()
+    matching_lists = []
 
-# Function to preprocess user input
-def preprocess_input(text):
-    tokens = nltk.word_tokenize(text.lower())
-    tokens = [token for token in tokens if token not in string.punctuation]
-    tokens = [token for token in tokens if token not in stop_words]
-    return tokens
+    if "meat" in user_input:
+        matching_lists.extend(meat_fish_keywords)
+    if "grains" in user_input:
+        matching_lists.extend(grains_keywords)
+    if "dairy" in user_input:
+        matching_lists.extend(dairy_keywords)
+    if any(word in user_input for word in ["fruits", "vegetables"]):
+        matching_lists.extend(fruits_vegetables_keywords)
+    if "baking" in user_input:
+        matching_lists.extend(baking_keywords)
+    if "beverages" in user_input:
+        matching_lists.extend(beverages_keywords)
+    if "sweet" in user_input:
+        matching_lists.extend(sweet_keywords)
+    if "sour" in user_input:
+        matching_lists.extend(sour_keywords)
+    if "spices" in user_input:
+        matching_lists.extend(spices_keywords)
 
-# Function to count matched ingredients
-def count_matched_ingredients(tokens, keywords):
-    set1 = set(tokens)
-    set2 = set(keywords)
+    return matching_lists if matching_lists else None
 
-    # Count the number of matches
-    count = len(set1.intersection(set2))
-    
-    return count
-
-def find_keywords(tokens, keywords):
-    matched_keywords = [token for token in tokens if token in keywords]
-    return matched_keywords
+#Seperate the words in a setting
+""" def toktok_tokenize(data):
+    toktok = nltk.ToktokTokenizer()
+    toktok.tokenize
+    toktok = [token for token in toktok if token not in string.punctuation]
+    #data = data.translate(str.maketrans('', '', string.punctuation))
+    #toktok = toktok.tokenize(data)
+    return toktok """
 
 #Recognize the shortcuts of a word
 def stem(tokens):
@@ -124,48 +135,99 @@ def filter_for_stop_words(tokens, stop_words):
             filtered.append(word)
     return filtered
 
+#Categorize the words to grammatical tagging
+def post_tagging(words):
+    tags = nltk.pos_tag(words)
+    nouns = [token for token, pos in tags if pos.startswith('N')]
+    verbs = [token for token, pos in tags if pos.startswith('V')]
+    adverbs = [token for token, pos in tags if pos.startswith('W')]
+    return nouns, verbs, adverbs
 
-# Main chat function
+#Processing the user inputs with the chatbot
+def preprocess_input(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [token for token in tokens if token not in string.punctuation]
+
+    tokens = [token for token in tokens if token.lower() not in stop_words]
+    return tokens
+
+def find_keywords(tokens, keywords):
+    matched_keywords = [token for token in tokens if token.capitalize() in keywords]
+    return matched_keywords
+
+# Seperate the words
+def toktok_tokenize_recipe(data):
+    toktok = nltk.ToktokTokenizer()
+    toktok_tokens = [toktok.tokenize(recipe['Step list']) for recipe in data]
+    return toktok_tokens
+
+# Count matched words
+def count_matched_words(tokens, keywords):
+    set1 = set(tokens)
+    set2 = set(keywords)
+
+    # Count the number of matches
+    count = len(set1.intersection(set2))
+    
+    return count
+
+def find_keywords(tokens, keywords):
+    matched_keywords = [token for token in tokens if token.capitalize() in keywords]
+    return matched_keywords
+    
+
+# Define a function to handle the conversation
 def chat():
-    print("Hello, I hope you are doing well! Welcome to your personal Recipe Finder!")
+    print("Helloooo, I hope you are doing well! Welcome to your personal Recipe Finder!")
     print("You can tell me which ingredients you have, and I can provide you with recipes based on those ingredients.")
     print("So let's start, what ingredients do you have?")
     print("Please write 'exit' when you are finished!")
-
-    # Load recipe data from CSV
     df = pd.read_csv('recipies.csv')
     data = df.to_dict(orient='records')
-
     while True:
         user_input = input("You: ").lower() 
+        
+       
+        processed_input = preprocess_input(user_input)
+        #toktok_tokenize(processed_input)
+        
+        # user_input = input("Enter keywords: ")
+        # keyword_list = find_keyword_list(user_input)
+
+        # if keyword_list:
+        #     print("Matching keywords:")
+        #     for keyword in keyword_list:
+        #         print(keyword)
+        # else:
+        #     print("No matching keyword list found for the input.")
+
+        filter_for_stop_words(processed_input, stop_words)
+        stem(processed_input)
+        lemmatize(processed_input)
+        post_tagging(processed_input)
+        print(processed_input)
+        matched_keywords = find_keywords(processed_input, 
+        meat_fish_keywords + grains_keywords + dairy_keywords + 
+        fruits_vegetables_keywords + baking_keywords + beverages_keywords + 
+        sweet_keywords + sour_keywords + spices_keywords)
+
+
 
         if user_input == 'exit':
             print("Thank you, enjoy your meal! Goodbye and I hope to see you soon!")
             break
-
-        processed_input = preprocess_input(user_input)
-        processed_input = filter_for_stop_words(processed_input, stop_words)
-        processed_input = stem(processed_input)
-        processed_input = lemmatize(processed_input)
-        keywords_all = meat_fish_keywords + grains_keywords + dairy_keywords + fruits_vegetables_keywords + baking_keywords + beverages_keywords + sweet_keywords + sour_keywords + spices_keywords
-        keywords_all = filter_for_stop_words(keywords_all, stop_words)
-        keywords_all = stem(keywords_all)
-        keywords_all = lemmatize(keywords_all)
-        # print("\n")
-        # print(keywords_all)
-        # print("\n")
-        processed_input = find_keywords(processed_input, keywords_all)
-
-        # print("\n")
-        # print(processed_input)
-        # print("\n")
-
-        if not processed_input:
+    
+    # Get keywords based on user input
+      
+        if matched_keywords is None:
             print("I'm sorry, I didn't understand that ingredient. Please try again.")
             continue
 
-        #print(f"The ingredients you mentioned are: {', '.join(processed_input)}")
+        print(f"The keywords for the '{user_input}' category are: {', '.join(matched_keywords)}")
 
+       
+
+        #DataCSV file
         
 
         max_match = 0
@@ -173,20 +235,35 @@ def chat():
         index_of_max = 0
 
         for i in data:
-            ingredient_string = i['Ingredient']
+            ingredient_string = i['Ingredient list']
             ingredient_token_list = preprocess_input(ingredient_string)
-            ingredient_token_list = filter_for_stop_words(ingredient_token_list, stop_words)
-            ingredient_token_list = stem(ingredient_token_list)
-            ingredient_token_list = lemmatize(ingredient_token_list)
+            filter_for_stop_words(ingredient_token_list, stop_words)
+            stem(ingredient_token_list)
+            lemmatize(ingredient_token_list)
+            post_tagging(ingredient_token_list)
 
-
-
-            match_counter = count_matched_ingredients(processed_input, ingredient_token_list)
+            match_counter = count_matched_words(matched_keywords, ingredient_token_list)
             if(max_match < match_counter):
                 max_match = match_counter
                 index_of_max = indexer
             indexer += 1
 
-        print("Based on your ingredients, here's a recipe recommendation:")
-        print(data[index_of_max]['Step'])
+        print(index_of_max)
+
+
+      
+        #print(data)
+        #Print the data out from the index
+        #data[0] 
+        
+        #How many Index positions do we have in the CSV 
+        #len(data)
+
+        if processed_input:
+            print("You mentioned:", ", ".join(processed_input))
+
+
+            print("Based on your ingredients, here's a recipe recommendation:")
+            print(data[index_of_max]['Step list'])
 chat()
+
